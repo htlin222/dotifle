eded70953d4783ff5e33a3f8f775d676423246ea
Lizard <ppoiu87@gmail.com>
2022-07-13 11:35:17 +0800

    update boots

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
+# install the brew 
+sh ~/.dotfile/Brewfile
+# rm the default config files
+rm -rf ~/.config
+# create link
 ln -s ~/.dotfile/.config ~/.config
 ln -s ~/.dotfile/.zshrc ~/.zshrc
 ln -s ~/.dotfile/.oh-my-zshrc ~/.oh-my-zshrc
